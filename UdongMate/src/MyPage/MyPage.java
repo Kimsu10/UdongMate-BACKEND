6c499a250e1e0bea47b0610bcbda43c6f4f4b2c3
kimsu10 <betnwjd25@gmail.com>
2024-04-13 23:55:07 +0900

    MODIFY: MyPage

--- UdongMate/src/MyPage/MyPage.java
+++ UdongMate/src/MyPage/MyPage.java
@@ -13,6 +13,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import JDBC.Jdbc;
+import Main.MainTap;
 
 public class MyPage extends MainTap {
    private static int loggedInUserNo;
@@ -35,19 +36,30 @@ public class MyPage extends MainTap {
    public MyPage() {
       // Call the constructor of the parent class
       super();
+      
+      setLocationRelativeTo(null);
+      
+      System.out.println(loggedInUserNo);
 
       contentPanel = new JPanel(new BorderLayout());
       getContentPane().add(contentPanel, BorderLayout.CENTER);
-      contentPanel.setBorder(new LineBorder(Color.BLACK));
+      
 
       JPanel leftPanel = new JPanel(new BorderLayout());
-      leftPanel.setBorder(new EmptyBorder(30, 150, 100, 100)); // 왼쪽 여백 설정
+      leftPanel.setBorder(new EmptyBorder(130, 200, 100, 100)); // 왼쪽 여백 설정
       contentPanel.add(leftPanel, BorderLayout.WEST);
 
       JPanel rightPanel = new JPanel(new BorderLayout());
-      rightPanel.setBorder(new EmptyBorder(10, 0, 0, 700)); // 오른쪽 여백 설정
+      rightPanel.setPreferredSize(new Dimension(1200, 1000)); // rightPanel 크기 설정
       contentPanel.add(rightPanel, BorderLayout.EAST);
+      
+      JPanel rPanel = new JPanel(new GridLayout(7, 1));
+      rightPanel.add(rPanel, BorderLayout.NORTH);
+      rightPanel.setBorder(new EmptyBorder(140, 450, 0, 200)); // 오른쪽 여백 설정
+      
 
+      Color menuItemTextColor = Color.GRAY;
+      
 
 
       try {
@@ -55,7 +67,11 @@ public class MyPage extends MainTap {
          PreparedStatement psmt = null;
          ResultSet rs = null;
 
-         String query = "SELECT * FROM user u, address a WHERE user_no = ?";
+         String query = "SELECT * FROM user u "
+               + "JOIN user_crew uc ON u.user_no = uc.user_no "
+               + "JOIN crew c ON uc.crew_no = c.crew_no "
+               + "JOIN address a ON u.addr_no = a.addr_no "
+               + "where u.user_no = ?";
          con = Jdbc.get();
          psmt = con.prepareStatement(query);
          psmt.setInt(1, loggedInUserNo);
@@ -63,45 +79,76 @@ public class MyPage extends MainTap {
 
          if (rs.next()) {
             // Retrieve user information
-            String userName = rs.getString("user_name");
-            String userNick = rs.getString("user_nick");
-            String userPhone = rs.getString("user_phone");
-            String userGender = rs.getString("user_gender");
-            int userAge = rs.getInt("user_age");
-            String addrName = rs.getString("addr_name");
-            String userImg = rs.getString("user_img");
+            String userName = rs.getString("u.user_name");
+            String userNick = rs.getString("u.user_nick");
+            String userPhone = rs.getString("u.user_phone");
+            String userGender = rs.getString("u.user_gender");
+            int userAge = rs.getInt("u.user_age");
+            String addrName = rs.getString("a.addr_name");
+            String userImg = rs.getString("u.user_img");
+            String crewName = rs.getString("c.crew_name");
+            int crewCap = rs.getInt("uc.crew_cap");
+            
 
             // Create labels for each user information
-            JLabel userNameLabel = new JLabel("이름 : " + userName);
-            JLabel userNickLabel = new JLabel("닉네임 : " + userNick);
-            JLabel userPhoneLabel = new JLabel("전화번호 : " + userPhone);
-            JLabel userGenderLabel = new JLabel("성별 : " + userGender);
-            JLabel userAgeLabel = new JLabel("나이 : " + userAge);
-            JLabel addrNoLabel = new JLabel("주소 : " + addrName);
+            JLabel userNameLabel = new JLabel("이름 :              " + userName);
+            JLabel userNickLabel = new JLabel("닉네임 :          " + userNick);
+            JLabel userPhoneLabel = new JLabel("전화번호 :       " + userPhone);
+            JLabel userGenderLabel = new JLabel("성별 :              " + userGender);
+            JLabel userAgeLabel = new JLabel("나이 :              " + userAge + "세");
+            JLabel addrNoLabel = new JLabel("주소 :              " + addrName);
             JLabel userImgLabel = new JLabel();
 
             // Add labels to the panel
             ImageIcon imageIcon = new ImageIcon(userImg);
             Image image = imageIcon.getImage();
             Image circularImage = getCircularImage(image);
-            userImgLabel.setIcon(new ImageIcon(circularImage)); // 등산 아이콘 이미지 설정
+            userImgLabel.setIcon(new ImageIcon(circularImage)); 
             leftPanel.add(userImgLabel, BorderLayout.NORTH);
 
             Font menuFont1 = new Font("Gong Gothic Bold", Font.PLAIN, 30);
-            JLabel groundLabel = new JLabel("산책/러닝"); // 버튼 아래에 표시될 텍스트
-            JLabel dLabel = new JLabel("d");
-            groundLabel.setFont(menuFont1);
-            groundLabel.setHorizontalAlignment(SwingConstants.CENTER); // 텍스트 가운데 정렬
-            rightPanel.add(groundLabel, BorderLayout.CENTER);
-            dLabel.setFont(menuFont1);
-            // Align text to the center
-            dLabel.setHorizontalAlignment(SwingConstants.CENTER);
+             
 
-            // Add labels to rightPanel
-            rightPanel.add(dLabel);
+            
+            userNickLabel.setFont(menuFont1);
+            userNameLabel.setFont(menuFont1);
+            userPhoneLabel.setFont(menuFont1);
+            userGenderLabel.setFont(menuFont1);
+            userAgeLabel.setFont(menuFont1);
+            addrNoLabel.setFont(menuFont1);
+            
+            userNameLabel.setForeground(menuItemTextColor);
+            userGenderLabel.setForeground(menuItemTextColor);
+            addrNoLabel.setForeground(menuItemTextColor);
+            
+            rPanel.add(userNickLabel);
+            rPanel.add(userNameLabel);
+            rPanel.add(userPhoneLabel);
+            rPanel.add(userGenderLabel);
+            rPanel.add(userAgeLabel);
+            rPanel.add(addrNoLabel);
+            
+            int verticalGap = 10;
+            userNickLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
+            userNameLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
+            userPhoneLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
+            userGenderLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
+            userAgeLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
+            addrNoLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
             
             
-            
+              
+            if(crewCap == 1) {               
+               JLabel crewNameLabel = new JLabel("크루 :              " + crewName + "(Leader)");
+               crewNameLabel.setFont(menuFont1);
+               rPanel.add(crewNameLabel);
+               crewNameLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
+            }else {
+               JLabel crewNameLabel = new JLabel("크루 :              " + crewName);   
+               crewNameLabel.setFont(menuFont1);
+               rPanel.add(crewNameLabel);
+               crewNameLabel.setBorder(new EmptyBorder(verticalGap, 0, verticalGap, 0));
+            }
             
             
          } else {
@@ -141,4 +188,3 @@ public class MyPage extends MainTap {
       return new Dimension(400, 300); // Set preferred size of the frame
    }
 }
-
