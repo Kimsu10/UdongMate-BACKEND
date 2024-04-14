c6417c767eba341fbeb8857280f9b22b4c931a8a
kimsu10 <betnwjd25@gmail.com>
2024-04-14 14:10:58 +0900

    ADD: Mountain dropButton

--- UdongMate/src/Main/Mountain.java
+++ UdongMate/src/Main/Mountain.java
@@ -1,5 +1,7 @@
 package Main;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 
 import javax.swing.*;
@@ -10,6 +12,8 @@ import JDBC.*;
 
 public class Mountain extends MainTap {
     private JPanel contentPane;
+    private JComboBox<String> addrDropdown;
+    private JComboBox<String> levDropdown;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -40,13 +44,158 @@ public class Mountain extends MainTap {
 
          try {
              Connection con = Jdbc.get();
+             
+             JPanel dropPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 210, 10)); // FlowLayout의 정렬 방식을 LEADING으로 설정
 
+             JPanel addrPanel = new JPanel(new FlowLayout(FlowLayout.LEADING)); // FlowLayout의 정렬 방식을 LEADING으로 설정
+             JLabel addrLabel = new JLabel("지역");
+             addrPanel.add(addrLabel);
+             String addr[] = {"선택안함", "종로구", "중구", "용산구", "성동구", "광진구", "동대문구", "중랑구", "성북구",
+                     "강북구", "도봉구", "노원구", "은평구", "서대문구", "마포구", "양천구", "강서구",
+                     "구로구", "금천구", "영등포구", "동작구", "관악구", "서초구", "강남구", "송파구", "강동구"};
+             addrDropdown = new JComboBox<>(addr);
+             addrPanel.add(addrDropdown);
+
+             JPanel levPanel = new JPanel(new FlowLayout(FlowLayout.LEADING)); // FlowLayout의 정렬 방식을 LEADING으로 설정
+             JLabel levLabel = new JLabel("난이도");
+             levPanel.add(levLabel);
+             String lev[] = {"선택안함", "하", "중", "상"};
+             levDropdown = new JComboBox<>(lev);
+             levPanel.add(levDropdown);
+
+             JButton searchButton = new JButton("검색");
+             searchButton.setPreferredSize(new Dimension(70, 30)); // 버튼 크기 설정
+
+             dropPanel.add(addrPanel);
+             dropPanel.add(levPanel);
+             dropPanel.add(searchButton); // 버튼 추가
+             
+             searchButton.addActionListener(new ActionListener() {
+                 @Override
+                 public void actionPerformed(ActionEvent a) {
+                     // 선택된 조건들을 가져옵니다.
+                     String selectedAddr = (String) addrDropdown.getSelectedItem();
+                     String selectedLev = (String) levDropdown.getSelectedItem(); // 선택된 난이도 가져오기
+                     
+
+                     try {
+                         Connection con = Jdbc.get();
+
+                         // SQL 쿼리
+                         String sql = "SELECT * FROM mountain JOIN address ON mountain.addr_no = address.addr_no ";
+                         String s[][] = {
+                               {"addr_name = ? "}, {"lev_no = ? "}
+                         };
+                         int c[]=  {0, 0};
+                         
+                         
+                         
+                         if(selectedAddr != "선택안함") c[0]++;
+                         if(selectedLev != "선택안함") c[1]++;
+                         
+                         if(c[0] == 1 || c[1] == 1) {
+                            sql += "WHERE ";
+                         }
+                         
+                         int cnt = 0;
+                        
+                         for(int i = 0;i < 2;i++) {
+                            if(c[i] == 1) cnt++;
+                         }
+                         
+                         for(int i = 0;i < 2;i++) {
+                            for(int j = 0;j < s[i].length;j++) {
+                               if(c[i] == 1) {
+                                  sql += s[i][j];
+                                  
+                                  if(cnt > 1) {
+                                       sql += "AND ";
+                                       cnt--;
+                                    }
+                                  
+                               }else break;
+                            }
+            
+                         }
+                         
+                         
+                         sql += "ORDER BY mt_dist ASC";
+
+                         
+                         PreparedStatement pstmt = con.prepareStatement(sql);
+                         
+                         for(int i = 1;i <= 2;i++) {
+                            if(c[0] == 1) {
+                               pstmt.setString(i, selectedAddr);
+                               c[0] = 0;
+                               continue;
+                            }
+                            if(c[1] == 1) {
+                               pstmt.setInt(i, getLevNo(selectedLev));
+                               c[1] = 0;
+                               continue;
+                            }
+                            
+                         }
+
+                         ResultSet rs = pstmt.executeQuery();
+
+                         // 기존 테이블 모델 초기화
+                         model.setRowCount(0);
+
+                         // 결과를 테이블 모델에 추가
+                         while (rs.next()) {
+                             // 나머지 코드는 동일합니다.
+                            String mtName = rs.getString("mt_name");
+                             String addrName = rs.getString("addr_name");
+                             int levNo = rs.getInt("lev_no");
+                             int mtDist = rs.getInt("mt_dist");
+                             double distance = Math.round(mtDist * 10) / 10.0;
+
+                             // 레벨 번호에 따라 문자열 설정
+                             String level;
+                                                    
+
+                             switch (levNo) {
+                                 case 1:
+                                     level = "하";
+                                     break;
+                                 case 2:
+                                     level = "중";
+                                     break;
+                                 case 3:
+                                     level = "상";
+                                     break;
+                                 default:
+                                     level = "알 수 없음";
+                             }
+
+                             // 데이터베이스에서 가져온 값을 테이블 모델에 추가
+                             model.addRow(new Object[]{mtName, distance + "km", addrName, level});
+                         }
+
+                         rs.close();
+                         pstmt.close();
+                         con.close();
+                     } catch (Exception e) {
+                         e.printStackTrace();
+                     }
+                 }
+             });
+             
+
+             
+
+             JScrollPane checkBoxScrollPane = new JScrollPane(dropPanel);
+             contentPane.add(checkBoxScrollPane, BorderLayout.NORTH);
+
+             
              // SQL 쿼리
-             String sql = "SELECT mountain.mt_name, mountain.mt_dist, address.addr_name, mountain.lev_no FROM mountain JOIN address ON mountain.addr_no = address.addr_no";
+             String sql = "SELECT * FROM mountain JOIN address ON mountain.addr_no = address.addr_no";
              PreparedStatement pstmt = con.prepareStatement(sql);
              ResultSet rs = pstmt.executeQuery();
 
-             // 결과를 테이블 모델에 추가
+             // 결과를 테이블 모델에 추가.
              while (rs.next()) {
                  String mtName = rs.getString("mt_name");
                  String addrName = rs.getString("addr_name");
@@ -202,5 +351,18 @@ public class Mountain extends MainTap {
 
         frame.add(detailsPanel);
         frame.setVisible(true);
+    }
+    
+    private int getLevNo(String lev) {
+        switch (lev) {
+            case "하":
+                return 1;
+            case "중":
+                return 2;
+            case "상":
+                return 3;
+            default:
+                return 0;
+        }
     }
 }
